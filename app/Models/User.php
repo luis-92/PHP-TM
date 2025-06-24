f65094dc0364008b6a2b98b5a03f981ec968afcb
Emanuel <manudatadev@icloud.com>
2025-06-24 17:47:41 -0600

    feat: creates user's table columns and implements attributes in its model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class User extends Model
 {
     //
+    protected $fillable = [
+        'name', 
+        'email',
+        'password'
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
 }
--- database/migrations/2025_06_24_175649_create_user_table.php
+++ database/migrations/2025_06_24_175649_create_user_table.php
@@ -12,7 +12,42 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('user', function (Blueprint $table) {
-            $table->id();
+            $table->id(); // Primary key
+            $table->string('name');
+            $table->string('first_last_name');
+            $table->string('second_last_name')->nullable();
+            $table->date('join_date');
+            // true: user is active | false: user is inactive 
+            $table->boolean('active');
+            $table->string('level')->nullable();
+            $table->string('phone_number')->nullable()->unique();
+            $table->string('gender')->nullable();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('country')->nullable();
+            $table->string('zip_code')->nullable();
+            $table->string('emergency_contact_name')->nullable();
+            $table->string('emergency_contact_phone')->nullable();
+            $table->string('occupation')->nullable();
+            $table->string('social_media_instagram')->nullable();
+            $table->string('social_media_fb')->nullable();
+            $table->string('social_media_tiktok')->nullable();
+            $table->string('achievements_interest')->nullable();
+            $table->text('professional_goals')->nullable();
+            $table->string('mentor_name')->nullable();
+            $table->boolean('is_mentor')->nullable();
+            $table->string('mentee_name')->nullable();
+            $table->string('user_type')->nullable();
+            $table->text('expected_objectives')->nullable();
+            $table->text('is_required_speak_job')->nullable();
+            $table->text('actual_level_speech')->nullable();
+            $table->text('actual_leadership_skills')->nullable();
+            $table->text('concerns_public_speak')->nullable();
+            $table->text('concerns_leadership')->nullable();
+            $table->text('why_join_toastmasters')->nullable();
             $table->timestamps();
         });
     }
